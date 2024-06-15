971e1d64122269c57d29ff0ee84630b41beff5bf
EkeminiThompson <ekemini.thompson@outlook.com>
2024-06-15 16:07:15 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
 
 db = SQLAlchemy()
 
@@ -10,17 +9,13 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        SQLALCHEMY_DATABASE_URI='sqlite:///{}'.format(os.path.join(app.instance_path, 'your_database.db')),
+        SQLALCHEMY_DATABASE_URI='sqlite:///{}'.format(app.instance_path + '/your_database.db'),
         SQLALCHEMY_TRACK_MODIFICATIONS=False
     )
     db.init_app(app)
 
-    # Ensure the instance directory exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    from . import routes, models  # Ensure this import is at the end
+    with app.app_context():
+        # Importing inside the function to avoid circular imports
+        from . import routes, models
 
     return app
