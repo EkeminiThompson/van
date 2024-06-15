7ab41434c718185ce342a92a0eb3b38d0f854d01
EkeminiThompson <ekemini.thompson@outlook.com>
2024-06-15 15:28:37 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import Config
+import os
 
-app = Flask(__name__)
-app.config.from_object(Config)
+app = Flask(__name__, instance_path='/tmp')
+
+# Update your configuration settings here, if needed
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tmp/test.db'  # Adjust this to your database URI
+
 db = SQLAlchemy(app)
 
-from app import routes
+from app import routes, models  # Ensure this import is at the end
